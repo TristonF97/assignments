1d20a671234188a23693d0f3d25536beb9da4b32
Triston Frischknecht <tristonfrish@gmail.com>
2020-11-24 09:18:17 -0700

    Node Calculator if else statements

--- Level-1/Node Calculator/app.js
+++ Level-1/Node Calculator/app.js
@@ -5,31 +5,31 @@ var num1 = readlineSync.question("Please enter your first number: ");
 var num2 = readlineSync.question("Please enter your second number: ");
 
 mathOperators = ["add", "sub", "mul", "div"],
-index = readlineSync.keyInSelect(mathOperators, "Please enter the operation to perform: add, sub, mul, div. ");
-    if (mathOperators[0] === add)
-        console.log("The result is: " + add)
-    if(mathOperators[1] === sub)
-        console.log("The result is: " + sub)
-    if(mathOperators[2] === mul)
-        console.log("The result is: " + mul)
-    if(mathOperators[3] === div){
-        console.log("The result is: " + div)
+index = readlineSync.keyInSelect(mathOperators, "Please enter the operation to perform: add, sub, mul, div.");
+    if (mathOperators[0] === "add")
+        console.log("The result is: " + add(num1, num2))
+    if(mathOperators[1] === "sub")
+        console.log("The result is: " + sub(num1, num2))
+    if(mathOperators[2] === "mul")
+        console.log("The result is: " + mul(num1, num2))
+    if(mathOperators[3] === "div"){
+        console.log("The result is: " + div(num1, num2))
     } else {
         console.log("Cancel")
     }
 
 function add(num1, num2) {
-    return (num1 + num2);
+    return +num1 + +num2;
 }
 
 function sub(num1, num2) {
-    return (num1 - num2);
+    return num1 - num2;
 }
 
 function mul(num1, num2) {
-    return (num1 * num2);
+    return num1 * num2;
 }
 
 function div(num1, num2) {
-    return (num1 / num2);
+    return num1 / num2;
 }
