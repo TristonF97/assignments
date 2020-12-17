1a51d866f783825ea7a640eaddc2d7aa12fef190
Triston Frischknecht <tristonfrish@gmail.com>
2020-12-17 09:26:40 -0700

    Pre Course Project

--- level-0/practice/index.js
+++
@@ -1,7 +0,0 @@
-var button = document.getElementById("button")
-
-function handleButtonClick() {
-    console.log("The button was clicked!")
-}
-
-button.addEventListener("click", handleButtonClick)
