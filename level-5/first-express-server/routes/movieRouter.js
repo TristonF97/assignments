aed1a68396184bad4b24b6190634557f8505a5d1
Triston Frischknecht <tristonfrish@gmail.com>
2021-03-22 09:22:07 -0600

    First Express Server

--- level-5/first-express-server/routes/movieRouter.js
+++ level-5/first-express-server/routes/movieRouter.js
@@ -32,7 +32,7 @@ movieRouter.post("/", (req, res) => {
     const newMovie = req.body
     newMovie._id = uuid()
     movies.push(newMovie)
-    res.send(`Successfully added ${newMovie.title} to the database.`)
+    res.send(newMovie)
 });
 
 // Delete One
--- level-5/first-express-server/server.js
+++ level-5/first-express-server/server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 
 // Middleware (for every request) //
-app.use(express.json()) // Looks for a request body, and turns it into 'req.body'
+app.use(express.json()) // Looks for a request body, and turns it into 'req.body'.
+app.use(morgan("dev")) // Logs requests to the console.
 
 app.use("/items", (req, res, next) => {
     console.log("THE ITEMS MIDDLEWARE WAS EXECUTED")
