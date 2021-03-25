84e5cea6a48aa7d9f2b5f2e7b1f527511f6639c9
Triston Frischknecht <tristonfrish@gmail.com>
2021-03-25 12:20:46 -0600

    DB Methods

--- level-5/book-server/server.js
+++ level-5/book-server/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 app.use(express.json())
 app.use(morgan('dev'))
 
-mongoose.connect("mongodb://localhost:27017/bookdb",
+mongoose.connect("mongodb://localhost:27017/db-methods",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -18,6 +18,11 @@ mongoose.connect("mongodb://localhost:27017/bookdb",
 
 app.use("/books", require("./routes/bookRouter.js"))
 app.use("/authors", require("./routes/authorRouter.js"))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
 
 app.listen(9000, () => {
     console.log("The server is running on Port 9000")
